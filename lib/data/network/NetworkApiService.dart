7bfabe4c940fcead65c8936dfc029ab6f29f12f1
Arooj Aslam <101873027+AroojAslam@users.noreply.github.com>
2025-03-16 08:38:13 +0500

    base api

---
+++ lib/data/network/NetworkApiService.dart
@@ -0,0 +1,50 @@
+import 'dart:convert';
+import 'dart:io';
+import 'package:http/http.dart' as http;
+import 'package:http/http.dart';
+import 'package:mvvm/data/app_exceptions.dart';
+import 'package:mvvm/data/network/BaseApiService.dart';
+
+class NetworkApiService extends BaseApiService {
+  @override
+  Future getResponse(String url) async {
+    dynamic responseJson;
+    try {
+      final response =
+          await http.get(Uri.parse(url)).timeout(const Duration(seconds: 10));
+      responseJson = returnResponse(response);
+    } on SocketException {
+      throw FetchDataException('No Internet Connection');
+    }
+    return responseJson;
+  }
+
+  @override
+  Future postResponse(String url, dynamic data) async {
+    dynamic responseJson;
+    try {
+      Response response = await post(Uri.parse(url), body: data)
+          .timeout(const Duration(seconds: 10));
+      responseJson = returnResponse(response);
+    } on SocketException {
+      throw FetchDataException('No Internet Connection');
+    }
+    return responseJson;
+  }
+
+  dynamic returnResponse(http.Response response) {
+    switch (response.statusCode) {
+      case 200:
+        dynamic responseJson = jsonDecode(response.body);
+        return responseJson;
+      case 400:
+        throw BadRequestException(response.body.toString());
+      case 404:
+        throw UnauthorisedException(response.body.toString());
+      default:
+        throw FetchDataException('Server error' +
+            'with status code' +
+            response.statusCode.toString());
+    }
+  }
+}
